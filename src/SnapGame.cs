5bee2d389205e6b9cd1e043e1ba917a6e8098bd2
Jcjx10 <jackchen000910@gmail.com>
2020-09-16 15:27:53 +0800

    add fonts

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -13,6 +13,10 @@ namespace CardGames
             SwinGame.BitmapSetCellDetails (cards, 82, 110, 13, 5, 53);      // set the cells in the bitmap to match the cards
         }
 
+		{
+			SwinGame.LoadFontNamed("GameFont","Chunckfive.otf",24)
+		}
+
 		/// <summary>
 		/// Respond to the user input -- with requests affecting myGame
 		/// </summary>
@@ -58,7 +62,7 @@ namespace CardGames
 			if (top != null)
 			{
 				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, 0, 20);
-				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue, 0, 30);
+				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.White,"GameFont", 0, 30);
 				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, 0, 40);
 				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 521, 153);
 			}
